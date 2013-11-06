28493ba0bcf1d4db85283f90ca03eeacb9289565
Meredith Taylor-Tanaka <mlttanaka@mochipuff.net>
2013-11-06 14:42:26 -0800

    Create Owner model which does not inherit from ActiveRecord. Say, whut?

---
+++ app/models/owner.rb
@@ -0,0 +1,21 @@
+class Owner
+  
+  def name
+    name = "Musa ni"
+  end
+  
+  def birthdate
+    birthdate = Date.new(2006, 7, 2)
+  end
+  
+  def countdown
+    today = Date.today
+    birthday = Date.new(today.year, birthdate.month, birthdate.day)
+    if birthday > today
+      countdown = (birthday - today).to_i
+    else
+      countdown = (birthday.next_year - today).to_i
+    end
+  end
+  
+end
