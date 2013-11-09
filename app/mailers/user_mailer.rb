c10eeb03baf6ef1f00e744fac661ab3160e2002b
Meredith Taylor-Tanaka <mlttanaka@mochipuff.net>
2013-11-08 18:46:26 -0800

    Update UserMailer default email address.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "do-not-reply@mochipuff.net"
+  default from: "do-not-reply@example.com"
   
   def contact_email(contact)
     @contact = contact
