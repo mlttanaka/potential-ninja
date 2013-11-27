61d23ed5fefdfa30a0f55ea96976848bb46c95df
Meredith Taylor-Tanaka <mlttanaka@mochipuff.net>
2013-11-26 21:15:45 -0800

    Make robots.txt aware of the Rails environment.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 LearnRails::Application.routes.draw do
   resources :contacts, only: [:new, :create]
   resources :visitors, only: [:new, :create]
+  
   root 'visitors#new'
+  
+  get '/robots.txt' => 'home#robots'
 end
